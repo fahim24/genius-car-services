67fdb139a1eba80d114ad9de5fec496db776d93a
aKai <fahim.ak24@gmail.com>
2022-04-17 03:33:47 +0600

    images added

---
+++ src/pages/Home/Service/Service.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Service.css";
+
+const Service = ({ service }) => {
+	const { name, price, description, img } = service;
+	return (
+		<div className="service">
+			<img src={img} alt="" />
+			<h3>{name}</h3>
+			<p>Price: {price}</p>
+			<p>
+				<small>{description}</small>
+			</p>
+			<button>Book: {name}</button>
+		</div>
+	);
+};
+
+export default Service;
