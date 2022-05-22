16058ca41db514e82f260f0c26cd75a7cfb2c7a5
aKai <fahim.ak24@gmail.com>
2022-05-22 21:29:15 +0600

    jwt

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,6 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Loading from "../Shared/Loading/Loading";
 import SocialLogin from "./SocialLogin/SocialLogin";
 import PageTitle from "../Shared/PageTitle/PageTitle";
+import axios from "axios";
 
 const Login = () => {
 	const emailRef = useRef("");
@@ -29,19 +30,22 @@ const Login = () => {
 	}
 
 	if (user) {
-		navigate(from, { replace: true });
+		// navigate(from, { replace: true });
 	}
 
 	if (error) {
 		errorElement = <p className="text-danger">Error: {error?.message}</p>;
 	}
 
-	const handleSubmit = (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
 		const email = emailRef.current.value;
 		const password = passwordRef.current.value;
 
-		signInWithEmailAndPassword(email, password);
+		await signInWithEmailAndPassword(email, password);
+		const { data } = await axios.post("http://localhost:5000/login", { email });
+		localStorage.setItem("accessToken", data.accessToken);
+		navigate(from, { replace: true });
 	};
 
 	const navigateRegister = (event) => {
